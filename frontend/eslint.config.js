92034f14c6c6dbcd69e3f00508f2378ff386fc53
JuanM0412 <juangomezp1204@gmail.com>
2025-06-03 14:55:47 -0500

    ci/cd: linters

---
+++ frontend/eslint.config.js
@@ -0,0 +1,32 @@
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import globals from 'globals'
+
+export default [
+  {
+    files: ['src/**/*.{ts,js,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: false },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      vue,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+]
